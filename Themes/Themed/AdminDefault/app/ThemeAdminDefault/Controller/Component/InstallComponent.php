6b6d6278b72398da0dfb273bbaff5fc60ece9f57
QuickApps <info@quickapps.es>
2011-11-07 01:19:15 +0100

    Update core themes so they can be used as Base themes for creating new ones.

---
+++ Themes/Themed/AdminDefault/app/ThemeAdminDefault/Controller/Component/InstallComponent.php
@@ -0,0 +1,18 @@
+<?php
+class InstallComponent extends Component {
+    public function beforeInstall(&$Installer) {
+        return true;
+    }
+
+    function afterInstall(&$Installer) {
+        return true;
+    }
+
+    public function beforeUninstall(&$Installer){
+        return true;
+    }
+
+    public function afterUninstall(&$Installer){
+        return true;
+    }
+}
