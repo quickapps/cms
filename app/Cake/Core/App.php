36df4ee5e8b933ad0181a0c4d20e6295c683bc03
QuickApps <info@quickapps.es>
2011-11-14 13:34:38 +0100

    Upgrade cakephp core to 2.0.3

--- app/Cake/Core/App.php
+++ app/Cake/Core/App.php
@@ -205,6 +205,7 @@ class App {
  * @param string $type type of path
  * @param string $plugin name of plugin
  * @return string array
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::path
  */
 	public static function path($type, $plugin = null) {
 		if (!empty(self::$legacy[$type])) {
@@ -236,6 +237,7 @@ class App {
  * use App::path()
  *
  * @return array An array of packages and their associated paths.
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::paths
  */
 	public static function paths() {
 		return self::$_packages;
@@ -259,6 +261,7 @@ class App {
  * @param array $paths associative array with package names as keys and a list of directories for new search paths
  * @param mixed $mode App::RESET will set paths, App::APPEND with append paths, App::PREPEND will prepend paths, [default] App::PREPEND
  * @return void
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::build
  */
 	public static function build($paths = array(), $mode = App::PREPEND) {
 		//Provides Backwards compatibility for old-style package names
@@ -323,6 +326,7 @@ class App {
  *
  * @param string $plugin CamelCased/lower_cased plugin name to find the path of.
  * @return string full path to the plugin.
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::pluginPath
  */
 	public static function pluginPath($plugin) {
 		return CakePlugin::path($plugin);
@@ -337,6 +341,7 @@ class App {
  *
  * @param string $theme theme name to find the path of.
  * @return string full path to the theme.
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::themePath
  */
 	public static function themePath($theme) {
 		$themeDir = 'Themed' . DS . Inflector::camelize($theme);
@@ -357,6 +362,7 @@ class App {
  *
  * @param string $type
  * @return string full path to package
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::core
  */
 	public static function core($type) {
 		return array(CAKE . str_replace('/', DS, $type) . DS);
@@ -383,6 +389,7 @@ class App {
  * @param mixed $path Optional Scan only the path given. If null, paths for the chosen type will be used.
  * @param boolean $cache Set to false to rescan objects of the chosen type. Defaults to true.
  * @return mixed Either false on incorrect / miss.  Or an array of found objects.
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::objects
  */
 	public static function objects($type, $path = null, $cache = true) {
 		$extension = '/\.php$/';
@@ -474,6 +481,7 @@ class App {
  * @param string $className the name of the class to configure package for
  * @param string $location the package name
  * @return void
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::uses
  */
 	public static function uses($className, $location) {
 		self::$_classMap[$className] = $location;
@@ -510,24 +518,24 @@ class App {
 		foreach ($paths as $path) {
 			$file = $path . $className . '.php';
 			if (file_exists($file)) {
-				//self::_map($file, $className);
 				self::_map($file, $className, $plugin);
 				return include $file;
 			}
 		}
 
-		//To help apps migrate to 2.0 old style file names are allowed
+		// To help apps migrate to 2.0 old style file names are allowed
+		// if the trailing segment is one of the types that changed, alternates will be tried.
 		foreach ($paths as $path) {
 			$underscored = Inflector::underscore($className);
 			$tries = array($path . $underscored . '.php');
 			$parts = explode('_', $underscored);
-			if (count($parts) > 1) {
+			$numParts = count($parts);
+			if ($numParts > 1 && in_array($parts[$numParts - 1], array('behavior', 'helper', 'component'))) {
 				array_pop($parts);
 				$tries[] = $path . implode('_', $parts) . '.php';
 			}
 			foreach ($tries as $file) {
 				if (file_exists($file)) {
-					//self::_map($file, $className);
 					self::_map($file, $className, $plugin);
 					return include $file;
 				}
@@ -542,6 +550,7 @@ class App {
  *
  * @param string $className name of the class to obtain the package name from
  * @return string package name or null if not declared
+ * @link http://book.cakephp.org/2.0/en/core-utility-libraries/app.html#App::location
  */
 	public static function location($className) {
 		if (!empty(self::$_classMap[$className])) {
@@ -668,7 +677,7 @@ class App {
  * @param boolean $return whether this function should return the contents of the file after being parsed by php or just a success notice
  * @return mixed if $return contents of the file after php parses it, boolean indicating success otherwise
  */
-	protected function _loadFile($name, $plugin, $search, $file, $return) {
+	protected static function _loadFile($name, $plugin, $search, $file, $return) {
 		$mapped = self::_mapped($name, $plugin);
 		if ($mapped) {
 			$file = $mapped;
@@ -783,6 +792,11 @@ class App {
 		return false;
 	}
 
+/**
+ * Sets then returns the templates for each customizable package path
+ * 
+ * @return array templates for each customizable package path
+ */
 	protected static function _packageFormat() {
 		if (empty(self::$_packageFormat)) {
 			self::$_packageFormat = array(
--- app/Cake/View/Helper/SessionHelper.php
+++ app/Cake/View/Helper/SessionHelper.php
@@ -100,6 +100,16 @@ class SessionHelper extends AppHelper {
  * echo $this->Session->flash('flash', array('element' => 'my_custom_element'));
  * }}}
  *
+ * If you want to use an element from a plugin for rendering your flash message you can do that using the 
+ * plugin param:
+ *
+ * {{{
+ * echo $this->Session->flash('flash', array(
+ *		'element' => 'my_custom_element',
+ *		'params' => array('plugin' => 'my_plugin')
+ * ));
+ * }}}
+ *
  * @param string $key The [Message.]key you are rendering in the view.
  * @param array $attrs Additional attributes to use for the creation of this flash message.
  *    Supports the 'params', and 'element' keys that are used in the helper.
@@ -128,9 +138,13 @@ class SessionHelper extends AppHelper {
 			} elseif ($flash['element'] == '' || $flash['element'] == null) {
 				$out = $message;
 			} else {
+				$options = array();
+				if (isset($flash['params']['plugin'])) {
+					$options['plugin'] = $flash['params']['plugin'];
+				}
 				$tmpVars = $flash['params'];
 				$tmpVars['message'] = $message;
-				$out = $this->_View->element($flash['element'], $tmpVars);
+				$out = $this->_View->element($flash['element'], $tmpVars, $options);
 			}
 			CakeSession::delete('Message.' . $key);
 		}
