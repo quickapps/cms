801f76564ce6f1c25b11282e16946aa99d2bbe69
QuickApps <info@quickapps.es>
2011-12-22 00:44:10 +0100

    Fix: moving `block` method to Block's Hooktag class.

--- app/Plugin/Block/View/Helper/BlockHookHelper.php
+++ app/Plugin/Block/View/Helper/BlockHookHelper.php
@@ -10,8 +10,7 @@
  * @link     http://cms.quickapps.es
  */
 class BlockHookHelper extends AppHelper {
-    // toolbar
-    public function beforeLayout($layoutFile) {
+    public function beforeLayout() {
         if (isset($this->request->params['plugin']) &&
             $this->request->params['plugin'] == 'block' &&
             $this->request->params['action'] != 'admin_add'
@@ -20,50 +19,5 @@ class BlockHookHelper extends AppHelper {
         }
 
         return true;
-    }
-
-    // hooktag, block rendering
-    public function block($options) {
-        extract($options);
-
-        if (!isset($id)) {
-            return;
-        }
-
-        if ($_block = Set::extract("/Block[id={$id}]/..", $this->_View->viewVars['Layout']['blocks'])) {
-            $block = $_block[0];
-        } else {
-            $block = ClassRegistry::init('Block.Block')->findById($id);
-        }
-
-        if (!$block) {
-            return;
-        }
-
-        $region = isset($region) ? $region : false;
-        $title = isset($title) ? int_val($title) : false;
-
-        return $this->_View->Layout->block($block, array('title' => $title, 'region' => $region));
-    }
-
-    // hooktag
-    public function block_title($options) {
-        extract($options);
-
-        if (!isset($id)) {
-            return;
-        }
-
-        if ($_block = Set::extract("/Block[id={$id}]/..", $this->_View->viewVars['Layout']['blocks'])) {
-            $block = $_block[0];
-        } else {
-            $block = ClassRegistry::init('Block.Block')->findById($id);
-        }
-
-        if (!$block) {
-            return false;
-        }
-
-        return $this->_View->Layout->hooktags($block['Block']['title']);
     }
 }
---
+++ app/Plugin/Block/View/Helper/BlockHooktagsHelper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Block Hooktags
+ *
+ * PHP version 5
+ *
+ * @package  QuickApps.Plugin.Block.View.Helper
+ * @version  1.0
+ * @author   Christopher Castro <chris@qucikapps.es>
+ * @link     http://cms.quickapps.es
+ */
+class BlockHooktagsHelper extends AppHelper {
+    public function block($options) {
+        extract($options);
+
+        if (!isset($id)) {
+            return;
+        }
+
+        if ($_block = Set::extract("/Block[id={$id}]/..", $this->_View->viewVars['Layout']['blocks'])) {
+            $block = $_block[0];
+        } else {
+            $block = ClassRegistry::init('Block.Block')->findById($id);
+        }
+
+        if (!$block) {
+            return;
+        }
+
+        $title = isset($title) ? $title : true;
+        $body = isset($body) ? $body : true;
+        $region = isset($region) ? $region : false;
+
+        return $this->_View->Layout->block($block,
+            array(
+                'title' => $title,
+                'body' => $body,
+                'region' => $region
+            )
+        );
+    }
+}
