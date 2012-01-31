ca6ebc49ccfc2a189e7a4acfdc2f3db67c216323
QuickApps <info@quickapps.es>
2012-01-31 10:42:36 +0100

    Update: minimum PHP version requirement to 5.2.8

--- app/Controller/InstallController.php
+++ app/Controller/InstallController.php
@@ -64,8 +64,8 @@ class InstallController extends Controller {
 
         $tests = array(
             'php' => array(
-                'test' => version_compare(PHP_VERSION, '5.2.6', '>='),
-                'msg' => __t('Your php version is not suported. check that your version is 5.2 or newer.')
+                'test' => version_compare(PHP_VERSION, '5.2.8', '>='),
+                'msg' => __t('Your php version is not suported. check that your version is 5.2.8 or newer.')
             ),
             'mysql' => array(
                 'test' => (extension_loaded('mysql') || extension_loaded('mysqli')),
