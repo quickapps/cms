1350e3a4b7e387babe9fd41a8c8acaa38236c039
QuickApps <info@quickapps.es>
2012-02-06 13:07:58 +0100

    Update: cakePHP core

--- app/Cake/Error/ExceptionRenderer.php
+++ app/Cake/Error/ExceptionRenderer.php
@@ -181,16 +181,10 @@ class ExceptionRenderer {
 			'url' => h($url),
 			'name' => $error->getMessage(),
 			'error' => $error,
-			'serialize' => array('code', 'url', 'name')
+			'_serialize' => array('code', 'url', 'name')
 		));
-		try {
-			$this->controller->set($error->getAttributes());
-			$this->_outputMessage($this->template);
-		} catch (MissingViewException $e) {
-			$this->_outputMessage('error500');
-		} catch (Exception $e) {
-			$this->_outputMessageSafe('error500');
-		}
+		$this->controller->set($error->getAttributes());
+		$this->_outputMessage($this->template);
 	}
 
 /**
@@ -210,7 +204,7 @@ class ExceptionRenderer {
 			'name' => $message,
 			'url' => h($url),
 			'error' => $error, 
-			'serialize' => array('name', 'url')
+			'_serialize' => array('name', 'url')
 		));
 		$this->_outputMessage('error400');
 	}
@@ -233,7 +227,7 @@ class ExceptionRenderer {
 			'name' => $message,
 			'message' => h($url),
 			'error' => $error,
-			'serialize' => array('name', 'message')
+			'_serialize' => array('name', 'message')
 		));
 		$this->_outputMessage('error500');
 	}
@@ -253,13 +247,9 @@ class ExceptionRenderer {
 			'url' => h($url),
 			'name' => $error->getMessage(),
 			'error' => $error,
-			'serialize' => array('code', 'url', 'name', 'error')
+			'_serialize' => array('code', 'url', 'name', 'error')
 		));
-		try {
-			$this->_outputMessage($this->template);
-		} catch (Exception $e) {
-			$this->_outputMessageSafe('error500');
-		}
+		$this->_outputMessage($this->template);
 	}
 
 /**
@@ -269,9 +259,13 @@ class ExceptionRenderer {
  * @return void
  */
 	protected function _outputMessage($template) {
-		$this->controller->render($template);
-		$this->controller->afterFilter();
-		$this->controller->response->send();
+		try {
+			$this->controller->render($template);
+			$this->controller->afterFilter();
+			$this->controller->response->send();
+		} catch (Exception $e) {
+			$this->_outputMessageSafe('error500');
+		}
 	}
 
 /**
@@ -282,8 +276,14 @@ class ExceptionRenderer {
  * @return void
  */
 	protected function _outputMessageSafe($template) {
+		$this->controller->layoutPath = '';
+		$this->controller->subDir = '';
+		$this->controller->viewPath = 'Errors/';
+		$this->controller->viewClass = 'View';
 		$this->controller->helpers = array('Form', 'Html', 'Session');
+
 		$this->controller->render($template);
+		$this->controller->response->type('html');
 		$this->controller->response->send();
 	}
 }
--- app/Cake/View/Helper/HtmlHelper.php
+++ app/Cake/View/Helper/HtmlHelper.php
@@ -18,6 +18,7 @@
  */
 
 App::uses('AppHelper', 'View/Helper');
+App::uses('CakeResponse', 'Network');
 
 /**
  * Html Helper class for easy use of HTML widgets.
@@ -73,6 +74,7 @@ class HtmlHelper extends AppHelper {
 		'tag' => '<%s%s>%s</%s>',
 		'tagstart' => '<%s%s>',
 		'tagend' => '</%s>',
+		'tagselfclosing' => '<%s%s/>',
 		'para' => '<p%s>%s</p>',
 		'parastart' => '<p%s>',
 		'label' => '<label for="%s"%s>%s</label>',
@@ -100,7 +102,8 @@ class HtmlHelper extends AppHelper {
  */
 	protected $_minimizedAttributes = array(
 		'compact', 'checked', 'declare', 'readonly', 'disabled', 'selected',
-		'defer', 'ismap', 'nohref', 'noshade', 'nowrap', 'multiple', 'noresize'
+		'defer', 'ismap', 'nohref', 'noshade', 'nowrap', 'multiple', 'noresize',
+		'autoplay', 'controls', 'loop', 'muted'
 	);
 
 /**
@@ -489,7 +492,7 @@ class HtmlHelper extends AppHelper {
  *
  * Add the script file to the `$scripts_for_layout` layout var:
  *
- * `$this->Html->script('styles.js', null, array('inline' => false));`
+ * `$this->Html->script('styles.js', array('inline' => false));`
  *
  * Add the script file to a custom block:
  *
@@ -565,11 +568,15 @@ class HtmlHelper extends AppHelper {
  * ### Options
  *
  * - `safe` (boolean) Whether or not the $script should be wrapped in <![CDATA[ ]]>
- * - `inline` (boolean) Whether or not the $script should be added to $scripts_for_layout or output inline
+ * - `inline` (boolean) Whether or not the $script should be added to
+ *   `$scripts_for_layout` / `script` block, or output inline. (Deprecated, use `block` instead)
+ * - `block` Which block you want this script block appended to.
+ *   Defaults to `script`.
  *
  * @param string $script The script to wrap
- * @param array $options The options to use.
- * @return mixed string or null depending on the value of `$options['inline']`
+ * @param array $options The options to use. Options not listed above will be
+ *    treated as HTML attributes.
+ * @return mixed string or null depending on the value of `$options['block']`
  * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/html.html#HtmlHelper::scriptBlock
  */
 	public function scriptBlock($script, $options = array()) {
@@ -577,14 +584,18 @@ class HtmlHelper extends AppHelper {
 		if ($options['safe']) {
 			$script  = "\n" . '//<![CDATA[' . "\n" . $script . "\n" . '//]]>' . "\n";
 		}
-		$inline = $options['inline'];
+		if (!$options['inline'] && empty($options['block'])) {
+			$options['block'] = 'script';
+		}
 		unset($options['inline'], $options['safe']);
-		$attributes = $this->_parseAttributes($options, ' ', ' ');
-		if ($inline) {
-			return sprintf($this->_tags['javascriptblock'], $attributes, $script);
+
+		$attributes = $this->_parseAttributes($options, array('block'), ' ');
+		$out = sprintf($this->_tags['javascriptblock'], $attributes, $script);
+
+		if (empty($options['block'])) {
+			return $out;
 		} else {
-			$this->_View->append('script', sprintf($this->_tags['javascriptblock'], $attributes, $script));
-			return null;
+			$this->_View->append($options['block'], $out);
 		}
 	}
 
@@ -756,8 +767,7 @@ class HtmlHelper extends AppHelper {
  *
  * - `url` If provided an image link will be generated and the link will point at
  *   `$options['url']`.
- * - `fullBase` If provided the src attribute will get a full addres (non-relative url) for 
- *   the image file.
+ * - `fullBase` If true the src attribute will get a full address for the image file.
  *
  * @param string $path Path to the image file, relative to the app/webroot/img/ directory.
  * @param array $options Array of HTML attributes.  See above for special options.
@@ -765,19 +775,8 @@ class HtmlHelper extends AppHelper {
  * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/html.html#HtmlHelper::image
  */
 	public function image($path, $options = array()) {
-		if (is_array($path)) {
-			$path = $this->url($path);
-		} elseif (strpos($path, '://') === false) {
-			if ($path[0] !== '/') {
-				$path = IMAGES_URL . $path;
-			}
-			$path = $this->assetTimestamp($this->webroot($path));
-		}
-		
-		if (!empty($options['fullBase'])) {
-			$path = $this->url('/', true) . $path;
-			unset($options['fullBase']);
-		}
+		$path = $this->assetUrl($path, $options + array('pathPrefix' => IMAGES_URL));
+		$options = array_diff_key($options, array('fullBase' => '', 'pathPrefix' => ''));
 
 		if (!isset($options['alt'])) {
 			$options['alt'] = '';
@@ -966,6 +965,86 @@ class HtmlHelper extends AppHelper {
 			$tag = 'para';
 		}
 		return sprintf($this->_tags[$tag], $this->_parseAttributes($options, null, ' ', ''), $text);
+	}
+
+/**
+ * Returns a VIDEO element
+ *
+ * ### Usage
+ *
+ * Using single video file:
+ *
+ * `echo $this->Html->video('video.mp4', array('fullBase' => true, 'text' => 'Fallback text'));`
+ *
+ * Outputs:
+ *
+ * `<video src="http://www.somehost.com/files/video.mp4">Fallback text</video>`
+ *
+ * Using multiple video files:
+ *
+ * {{{
+ * echo $this->Html->video(
+ * 		array('video.mp4', array('src' => 'video.ogg', 'type' => "video/ogg; codecs='theora, vorbis'")),
+ * 		array('autoplay')
+ * );
+ * }}}
+ *
+ * Outputs:
+ *
+ * {{{
+ * <video autoplay="autoplay">
+ * 		<source src="/files/video.mp4" type="video/mp4"/>
+ * 		<source src="/files/video.ogg" type="video/ogg; codecs='theora, vorbis'"/>
+ * </video>
+ * }}}
+ *
+ * ### Options
+ *
+ * - `text` Text to include inside the video tag
+ * - `pathPrefix` Path prefix to use for relative urls, defaults to 'files/'
+ * - `fullBase` If provided the src attribute will get a full address including domain name
+ *
+ * @param string|array $path Path to the video file, relative to the webroot/{$options['pathPrefix']} directory.
+ *  Or an array where each item itself can be a path string or an associate array containing keys `src` and `type`
+ * @param array $options Array of HTML attributes, and special options above.
+ * @return string Generated video tag
+ */
+	public function video($path, $options = array()) {
+		$options += array('pathPrefix' => 'files/', 'text' => '');
+
+		if (is_array($path)) {
+			$response = null;
+			$sourceTags = '';
+			foreach ($path as $source) {
+				if (is_string($source)) {
+					$source = array(
+						'src' => $source,
+					);
+				}
+				if (!isset($source['type'])) {
+					if ($response === null) {
+						$response = new CakeResponse();
+					}
+					$source['type'] = $response->getMimeType(pathinfo($source['src'], PATHINFO_EXTENSION));
+				}
+				$source['src'] = $this->assetUrl($source['src'], $options);
+				$sourceTags .= $this->useTag('tagselfclosing', 'source', $source);
+			}
+			$options['text'] = $sourceTags . $options['text'];
+			unset($options['fullBase']);
+		} else {
+			$path = $this->assetUrl($path, $options);
+			$options['src'] = $path;
+		}
+
+		if (isset($options['poster'])) {
+			$options['poster'] = $this->assetUrl($options['poster'], array('pathPrefix' => IMAGES_URL) + $options);
+		}
+
+		$text = $options['text'];
+
+		$options = array_diff_key($options, array('fullBase' => '', 'pathPrefix' => '', 'text' => ''));
+		return $this->tag('video', $text, $options);
 	}
 
 /**
