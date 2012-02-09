b7bea5fef9a1ade8b3f56928a4ec27f988276f2c
QuickApps <info@quickapps.es>
2012-02-09 15:59:49 +0100

    Update: ignore list & site bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,7 +1,7 @@
-<?php 
-/**	
+<?php
+/**
  * You can use this file to set specific logic outside the QuickApps CMS core,
- * so updates won't affect them. Warning: This is for advanced users only, those with a 
+ * so updates won't affect them. Warning: This is for advanced users only, those with a
  * proper understanding of PHP and its syntax.
  *
  * NOTE: This file is included after QuickApps CMS bootstrap.php file.
