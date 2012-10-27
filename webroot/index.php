693bdc3c3588c4ed98fa7e491b300ce7a898a1fc
QuickApps <info@quickapps.es>
2012-10-27 22:24:19 +0200

    Update: webroot/index.php

--- webroot/index.php
+++ webroot/index.php
@@ -4,44 +4,107 @@
  *
  * The Front Controller for handling every request
  *
- * @package       webroot
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.webroot
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-    define('DS', DIRECTORY_SEPARATOR);
-    define('ROOT', dirname(dirname(__FILE__)));
-    define('QA_PATH', ROOT . DS . 'QuickApps');
+/**
+ * Use the DS to separate the directories in other defines
+ */
+if (!defined('DS')) {
+	define('DS', DIRECTORY_SEPARATOR);
+}
+/**
+ * These defines should only be edited if you have cake installed in
+ * a directory layout other than the way it is distributed.
+ * When using custom settings be sure to use the DS and do not add a trailing DS.
+ */
+
+/**
+ * The full path to the directory which holds "app", WITHOUT a trailing DS.
+ *
+ */
+if (!defined('ROOT')) {
+	define('ROOT', dirname(dirname(__FILE__)));
+}
+
+/**
+ * Path to the temporary files directory.
+ */
+if (!defined('TMP')) {
+	define('TMP', ROOT . DS . 'tmp' . DS);
+}
+
+/**
+ * The full path to the QuickApps CMS core, WITHOUT a trailing DS.
+ *
+ * Change this value for shared core installation.
+ *
+ */
+if (!defined('QA_PATH')) {
+	define('QA_PATH', ROOT . DS . 'QuickApps');
+}
+
+/**
+ * The actual directory name for the "app".
+ *
+ */
+if (!defined('APP_DIR')) {
+	define('APP_DIR', basename(QA_PATH));
+}
+
+/**
+ * The absolute path to the "cake" directory, WITHOUT a trailing DS.
+ *
+ * Un-comment this line to specify a fixed path to CakePHP.
+ * This should point at the directory containing `Cake`.
+ *
+ * For ease of development CakePHP uses PHP's include_path.  If you
+ * cannot modify your include_path set this value.
+ *
+ * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
+ */
+	define('CAKE_CORE_INCLUDE_PATH', QA_PATH);
 
 /**
  * Editing below this line should NOT be necessary.
  * Change at your own risk.
  *
  */
-    define('APP_DIR', dirname(QA_PATH));
-    define('CAKE_CORE_INCLUDE_PATH', dirname(dirname(QA_PATH)));
-    define('TMP', ROOT . DS . 'tmp' . DS);
-    define('APP', QA_PATH . DS);
+if (!defined('WEBROOT_DIR')) {
+	define('WEBROOT_DIR', basename(dirname(__FILE__)));
+}
+if (!defined('WWW_ROOT')) {
+	define('WWW_ROOT', dirname(__FILE__) . DS);
+}
 
-    if (!defined('WEBROOT_DIR')) {
-        define('WEBROOT_DIR', basename(dirname(__FILE__)));
-    }
+if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+	if (function_exists('ini_set')) {
+		ini_set('include_path', ROOT . DS . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+	}
+	if (!include ('Cake' . DS . 'bootstrap.php')) {
+		$failed = true;
+	}
+} else {
+	if (!include (CAKE_CORE_INCLUDE_PATH . DS . 'Cake' . DS . 'bootstrap.php')) {
+		$failed = true;
+	}
+}
+if (!empty($failed)) {
+	trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
+}
 
-    if (!defined('WWW_ROOT')) {
-        define('WWW_ROOT', dirname(__FILE__) . DS);
-    }
+App::uses('Dispatcher', 'Routing');
 
-    if (!defined('CORE_PATH')) {
-        define('APP_PATH', QA_PATH . DS);
-        define('CORE_PATH', APP_PATH);
-    }
-
-    if (!include(CORE_PATH . 'Cake' . DS . 'bootstrap.php')) {
-        trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
-    }
-
-    if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO'] == '/favicon.ico') {
-        return;
-    }
-
-    App::uses('Dispatcher', 'Routing');
-
-    $Dispatcher = new Dispatcher();
-    $Dispatcher->dispatch(new CakeRequest(), new CakeResponse(array('charset' => Configure::read('App.encoding'))));
+$Dispatcher = new Dispatcher();
+$Dispatcher->dispatch(new CakeRequest(), new CakeResponse(array('charset' => Configure::read('App.encoding'))));
