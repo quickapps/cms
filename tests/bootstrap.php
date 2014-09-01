f5a4bc60b27fcd8a62a75be3ba36ad2a38728494
QuickApps <info@quickapps.es>
2014-09-01 02:27:07 +0200

    more travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -115,7 +115,6 @@ function snapshot() {
 	$snapshot['plugins'] = Hash::sort($snapshot['plugins'], '{s}.name', 'asc');
 	Configure::write('QuickApps', $snapshot);
 	Configure::dump('snapshot.php', 'QuickApps', ['QuickApps']);
-	debug($snapshot);
 }
 
 require QA_CORE . '/config/bootstrap.php';
