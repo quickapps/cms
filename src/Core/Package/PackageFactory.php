8bf8c1f7988d7a39b653aea3391c0e8f58ffe7f1
QuickApps <info@quickapps.es>
2015-03-09 17:45:34 +0100

    PackageFactory improved
    New detection methods can be registered on this class using
    `addDetector()` method. This provides support for new package types on
    the future

--- src/Core/Package/PackageFactory.php
+++ src/Core/Package/PackageFactory.php
@@ -11,6 +11,7 @@
  */
 namespace QuickApps\Core\Package;
 
+use QuickApps\Core\Package\BasePackage;
 use QuickApps\Core\Package\GenericPackage;
 use QuickApps\Core\Package\LibraryPackage;
 use QuickApps\Core\Package\PluginPackage;
@@ -18,34 +19,140 @@ use QuickApps\Core\Package\ThirdPartyPackage;
 use QuickApps\Core\Plugin;
 
 /**
- * Used to create package objects instances.
+ * Used to create package objects.
  *
+ * In QuickAppsCMS plugins and themes are internally handled as "packages",
+ * there exists a few packages types, for example third-party libraries which are
+ * installed through composer's install command. Each package type is represented
+ * by its own class, QuickAppsCMS comes with a few of this classes: `PluginPackage`,
+ * `LibraryPackage`, `ThirdPartyPackage` and `GenericPackage`. These classes
+ * provides a set of useful methods for interacting with QuickAppsCMS.
+ *
+ * This class automatically tries to determinate the best package type based on
+ * its name using a what we call "detectors" methods. This class comes with a few
+ * built-in detector methods which are described below, however more detectors can
+ * be registered (or overwrite existing ones) using the `addDetector()` method. A
+ * "detector" is a simple callable function which based on a given package name it
+ * should return an object representing that package if the given package name
+ * matches the type of package the detector represents. For example:
+ *
+ *
+ * ### Registering detectors:
+ *
+ * ```php
+ * PackageFactory::addDetector('myVendorPlugin', function ($packageName) {
+ *     list($vendor, $package) = packageSplit($packageName);
+ *     if ($vendor == 'my-vendor-plugin') {
+ *         return new MyVendorPlugin($package, "/path/to/{$package}/")
+ *     }
+ * });
+ * ```
+ *
+ * In this example we are using our own `MyVendorPlugin` class for representing
+ * packages created by `my-vendor-plugin`.
+ *
+ * ### Built-in detectors:
+ *
+ * - plugin: For packages representing QuickAppsCMS plugins.
+ *
+ * - library: For packages representing PHP extension libraries or PHP itself, for
+ *   example: `ext-intl`, `php`, `ext-zlib`, etc
+ *
+ * - thirdParty: For packages representing third-party libraries installed used
+ *   composer, for example: `nesbot/carbon`, `robmorgan/phinx`, etc.
+ *
+ * ### Detection order:
+ *
+ * Detectors methods are invoked in the order they were registered, if one detector
+ * fails to detect a package the next registered detector will be used, and so on.
  */
 class PackageFactory
 {
 
     /**
+     * List of detectors methods indexed as `name` => `callable`.
+     *
+     * @var array
+     */
+    protected static $_detectors = [];
+
+    /**
+     * Indicates if default detectors were initialized.
+     *
+     * @var bool
+     */
+    protected static $_initialized = false;
+
+    /**
      * Given a full package name, returns an instance of an object representing
      * that package.
+     *
+     * If no matching package is found, `GenericPackage` will be used by default.
      *
      * @param string $package Full package name. e.g. `vendor-name/package-name`
      * @return \QuickApps\Core\Package\BasePackage
      */
     public static function create($package)
     {
-        if ($plugin = static::_getPlugin($package)) {
-            return $plugin;
+        static::_init();
+        foreach (static::$_detectors as $name => $callable) {
+            $result = $callable($package);
+            if ($result instanceof BasePackage) {
+                return $result;
+            }
         }
-
-        if ($lib = static::_getLibrary($package)) {
-            return $lib;
-        }
-
-        if ($third = static::_getThirdParty($package)) {
-            return $third;
-        }
-
         return new GenericPackage($package, '', '');
+    }
+
+    /**
+     * Initializes this class.
+     *
+     * @return void
+     */
+    protected static function _init()
+    {
+        if (static::$_initialized) {
+            return;
+        }
+
+        static::$_detectors['plugin'] = function ($package) {
+            return static::_getPlugin($package);
+        };
+
+        static::$_detectors['library'] = function ($package) {
+            return static::_getLibrary($package);
+        };
+
+        static::$_detectors['thirdParty'] = function ($package) {
+            return static::_getThirdParty($package);
+        };
+
+        static::$_initialized = true;
+    }
+
+    /**
+     * Registers a new package getter method.
+     *
+     * Callable function should return an object package extending
+     * `QuickApp\Core\Package\BasePackage` class on success.
+     *
+     * @param string $name The name for this detector
+     * @param callable $method The callable method
+     * @return void
+     */
+    public static function addDetector($name, callable $method)
+    {
+        static::$_detectors[$name] = $callable;
+    }
+
+    /**
+     * Gets a list of all registered detectors.
+     *
+     * @return array
+     */
+    public static function detectors()
+    {
+        return static::$_detectors;
     }
 
     /**
