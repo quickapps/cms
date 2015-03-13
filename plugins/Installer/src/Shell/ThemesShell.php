22d74b4f4437b588967b2a3fb5074171854eedde
QuickApps <info@quickapps.es>
2015-03-13 17:05:03 +0100

    ICU messages

--- plugins/Installer/src/Shell/ThemesShell.php
+++ plugins/Installer/src/Shell/ThemesShell.php
@@ -74,15 +74,15 @@ class ThemesShell extends Shell
      */
     public function main()
     {
-        $this->out('<info>Themes Shell</info>');
+        $this->out(__d('installer', '<info>Themes Shell</info>'));
         $this->hr();
-        $this->out('[I]nstall new theme');
-        $this->out('[R]emove an existing theme');
-        $this->out('[C]hange site theme');
-        $this->out('[H]elp');
-        $this->out('[Q]uit');
+        $this->out(__d('installer', '[I]nstall new theme'));
+        $this->out(__d('installer', '[R]emove an existing theme'));
+        $this->out(__d('installer', '[C]hange site theme'));
+        $this->out(__d('installer', '[H]elp'));
+        $this->out(__d('installer', '[Q]uit'));
 
-        $choice = strtolower($this->in('What would you like to do?', ['I', 'R', 'C', 'H', 'Q']));
+        $choice = strtolower($this->in(__d('installer', 'What would you like to do?'), ['I', 'R', 'C', 'H', 'Q']));
         switch ($choice) {
             case 'i':
                 $this->_install();
@@ -99,7 +99,7 @@ class ThemesShell extends Shell
             case 'q':
                 return $this->_stop();
             default:
-                $this->out('You have made an invalid selection. Please choose a command to execute by entering 1, 2, 3, H, or Q.');
+                $this->out(__d('installer', 'You have made an invalid selection. Please choose a command to execute by entering I, R, C, H, or Q.'));
         }
         $this->hr();
         $this->main();
@@ -142,23 +142,23 @@ class ThemesShell extends Shell
      */
     protected function _install()
     {
-        $message = "Please provide a theme source, it can be either an URL or a filesystem path to a ZIP/directory within your server?\n[Q]uit";
+        $message = __d('installer', "Please provide a theme source, it can be either an URL or a filesystem path to a ZIP/directory within your server?\n[Q]uit");
 
         while (true) {
             $source = $this->in($message);
             if (strtoupper($source) === 'Q') {
-                $this->err('Installation aborted');
+                $this->err(__d('installer', 'Installation aborted'));
                 break;
             } else {
-                $this->out('Starting installation...', 0);
+                $this->out(__d('installer', 'Starting installation...'), 0);
                 $task = $this->dispatchShell("Installer.plugins install -s \"{$source}\" --theme -a");
 
                 if ($task === 0) {
-                    $this->_io->overwrite('Starting installation... successfully installed!', 2);
+                    $this->_io->overwrite(__d('installer', 'Starting installation... successfully installed!'), 2);
                     $this->out();
                     break;
                 } else {
-                    $this->_io->overwrite('Starting installation... failed!', 2);
+                    $this->_io->overwrite(__d('installer', 'Starting installation... failed!'), 2);
                     $this->out();
                 }
             }
@@ -183,43 +183,43 @@ class ThemesShell extends Shell
         $this->out();
         foreach ($allThemes as $plugin) {
             $allThemes[$index] = $plugin;
-            $this->out(__d('installer', '[{0,number}] {1}', $index, $plugin->human_name));
+            $this->out(__d('installer', '[{index, number}] {name}', ['index' => $index, 'name' => $plugin->human_name]));
             $index++;
         }
         $this->out();
 
-        $message = "Which theme would you like to uninstall?\n[Q]uit";
+        $message = __d('installer', "Which theme would you like to uninstall?\n[Q]uit");
         while (true) {
             $in = trim($this->in($message));
             if (strtoupper($in) === 'Q') {
-                $this->err('Operation aborted');
+                $this->err(__d('installer', 'Operation aborted'));
                 break;
             } elseif (intval($in) < 1 || !isset($allThemes[intval($in)])) {
-                $this->err('Invalid option');
+                $this->err(__d('installer', 'Invalid option'));
             } else {
                 $plugin = Plugin::get($allThemes[$in]->name());
                 $this->hr();
-                $this->out('<info>The following theme will be uninstalled</info>');
+                $this->out(__d('installer', '<info>The following theme will be uninstalled</info>'));
                 $this->hr();
-                $this->out(sprintf('Name:        %s', $plugin->name));
-                $this->out(sprintf('Description: %s', $plugin->composer['description']));
-                $this->out(sprintf('Path:        %s', $plugin->path));
+                $this->out(__d('installer', 'Name:        {0}', $plugin->name));
+                $this->out(__d('installer', 'Description: {0}', $plugin->composer['description']));
+                $this->out(__d('installer', 'Path:        {0}', $plugin->path));
                 $this->hr();
                 $this->out();
 
-                $confirm = $this->in(sprintf('Please type in "%s" to uninstall', $allThemes[$in]->name));
+                $confirm = $this->in(__d('installer', 'Please type in "{0}" to uninstall', $allThemes[$in]->name));
                 if ($confirm === $allThemes[$in]->name) {
                     $task = $this->dispatchShell("Installer.plugins uninstall -p {$allThemes[$in]->name}");
 
                     if ($task === 0) {
-                        $this->out('Plugin uninstalled!');
+                        $this->out(__d('installer', 'Plugin uninstalled!'));
                         Plugin::dropCache();
                     } else {
-                        $this->err('Plugin could not be uninstalled.', 2);
+                        $this->err(__d('installer', 'Plugin could not be uninstalled.'), 2);
                         $this->out();
                     }
                 } else {
-                    $this->err('Confirmation failure, operation aborted!');
+                    $this->err(__d('installer', 'Confirmation failure, operation aborted!'));
                 }
                 break;
             }
@@ -242,7 +242,7 @@ class ThemesShell extends Shell
             ->toArray();
 
         if (!count($disabledThemes)) {
-            $this->err('<info>There are no disabled themes!</info>');
+            $this->err(__d('installer', '<info>There are no disabled themes!</info>'));
             $this->out();
             return;
         }
@@ -251,27 +251,27 @@ class ThemesShell extends Shell
         $this->out();
         foreach ($disabledThemes as $theme) {
             $disabledThemes[$index] = $theme;
-            $this->out(__d('installer', '[{0,number,integer}] {1} [{2}]', $index, $theme->human_name, $theme->isAdmin ? 'back' : 'front'));
+            $this->out(__d('installer', '[{index, number, integer}] {name} [{theme_type, select, front{frontend} other{backend}}]', ['index' => $index, 'name' => $theme->human_name, 'theme_type' => ($theme->isAdmin ? 'back' : 'front')]));
             $index++;
         }
         $this->out();
 
-        $message = "Which theme would you like to activate?\n[Q]uit";
+        $message = __d('installer', "Which theme would you like to activate?\n[Q]uit");
         while (true) {
             $in = $this->in($message);
             if (strtoupper($in) === 'Q') {
-                $this->err('Operation aborted');
+                $this->err(__d('installer', 'Operation aborted'));
                 break;
             } elseif (intval($in) < 1 || !isset($disabledThemes[intval($in)])) {
-                $this->err('Invalid option');
+                $this->err(__d('installer', 'Invalid option'));
             } else {
                 $task = $this->dispatchShell("Installer.themes change -t {$disabledThemes[$in]->name}");
 
                 if ($task === 0) {
-                    $this->out('Theme changed!');
+                    $this->out(__d('installer', 'Theme changed!'));
                     Plugin::dropCache();
                 } else {
-                    $this->err('Theme could not be changed.', 2);
+                    $this->err(__d('installer', 'Theme could not be changed.'), 2);
                     $this->out();
                 }
                 break;
