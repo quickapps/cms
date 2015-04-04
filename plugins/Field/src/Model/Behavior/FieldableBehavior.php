d6dfc031f2881845d3bc3ff43434079d2f33f59f
QuickApps <info@quickapps.es>
2015-04-04 23:15:38 +0200

    remove documentation
    added link to online documentation

--- plugins/Field/src/Model/Behavior/FieldableBehavior.php
+++ plugins/Field/src/Model/Behavior/FieldableBehavior.php
@@ -26,6 +26,7 @@ use Field\Error\InvalidBundle;
 use Field\Model\Entity\Field;
 use Field\Model\Entity\FieldValue;
 use QuickApps\Event\HookAwareTrait;
+use \ArrayObject;
 
 /**
  * Fieldable Behavior.
@@ -41,402 +42,7 @@ use QuickApps\Event\HookAwareTrait;
  *   tuple of your Table. (Schema equivalent: cell value)
  *
  * **This behavior allows you to add _virtual columns_ to your table schema.**
- *
- *
- * ***
- *
- *
- * This behavior modifies each query of your table in order to merge custom-fields
- * records into each entity under the `_fields` property.
- *
- * ## Entity Example:
- *
- * ```php
- * $user = $this->Users->get(1);
- * // User's properties might look as follows:
- * [id] => 1,
- * [password] => e10adc3949ba59abbe56e057f20f883e,
- * ...
- * [_fields] => [
- *      [0] => [
- *          [name] => user-age,
- *          [label] => User Age,
- *          [value] => 22,
- *          [extra] => null,
- *          [metadata] => [ ... ]
- *      ],
- *      [1] => [
- *           [name] => user-phone,
- *           [label] => User Phone,
- *           [value] => null, // no data stored
- *           [extra] => null, // no data stored
- *           [metadata] => [ ... ]
- *      ],
- *      ...
- *      [n] => [ ... ]
- * ]
- * ```
- *
- * In the example above, User entity has a custom field named `user-age` and its
- * current value is 22. In the other hand, it also has a `user-phone` field but
- * no information was given (Schema equivalent: NULL cell).
- *
- * As you might see, the `_field` key contains an array list of all fields attached
- * to every entity. Each field (each element under the `_field` key) is an object
- * (Field Entity), and it have a number of properties such as `label`, `value`, etc.
- * All properties are described below:
- *
- * -  `name`: Machine-name of this field. ex. `article-body` (Schema equivalent: column name).
- * -  `label`: Human readable name of this field e.g.: `User Last name`.
- * -  `value`: Value for this [field, entity] tuple. (Schema equivalent: cell value)
- * -  `extra`: Additional information.
- * -  `metadata`: Metadata (an Entity Object).
- *     - `field_value_id`: ID of the value stored in `field_values` table.
- *     - `field_instance_id`: ID of field instance (`field_instances` table)
- *        attached to the table.
- *     - `entity_id`: ID of the Entity this field is attached to.
- *     - `table_alias`: Name of the table this field is attached to. e.g: `users`.
- *     - `description`: Something about this field: e.g.: `Please enter your last name`.
- *     - `required`: 0|1.
- *     - `settings`: Any extra information array handled by this particular field.
- *     - `view_modes`: Information about how this field should be rendered on each
- *        View Mode. Information is stored as `view-mode-name` => `rendering-information`.
- *     - `handler`: Name of the Field Handler (without namespace). e.g.
- *       `TaxonomyField` for `Taxonomy\Event\TaxonomyField` class.
- *     - `errors`: Array of validation error messages, only on edit mode.
- *     - `entity`: Entity object this field is attached to.
- *
- * **Notes:**
- *
- * -    The `metadata` key on every field is actually an entity object.
- * -    The `_field` key which holds all the fields is actually an instance of
- *      `Field/Collection/FieldCollection`, which behaves as an array
- *      (so you can iterate over it). It adds some utility methods for handling
- *      fields, for instance it allows you to access an specific field by its
- *      corresponding numeric index or by its machine-name.
- *
- * ### Accessing Field Properties
- *
- * Once you have your Entity (e.g. User Entity), you would probably need to get
- * its attached fields and do fancy thing with them. Following with our User
- * entity example:
- *
- * ```php
- * // In your controller
- * $user = $this->Users->get($id);
- * echo $user->_fields[0]->label . ': ' . $user->_fields[0]->value;
- * // out: User Age: 22
- *
- * echo "This field is attached to '" . $user->_fields[0]->metadata->table_alias . "' table";
- * // out: This field is attached to 'users' table;
- * ```
- *
- * ## Searching over custom fields
- *
- * This behavior allows you to perform WHERE clauses using any of the fields
- * attached to your table. Every attached field has a "machine-name"
- * (a.k.a. field slug), you should use this "machine-name" prefixed with `:`,
- * for example:
- *
- * ```php
- * TableRegistry::get('Users')
- *   ->find('all')
- *   ->where(['Users.:first-name LIKE' => 'John%']);
- * ```
- *
- * In this example the `Users` table has a custom field attached (first-name), and
- * we are looking for all the users whose `first-name` starts with `John`.
- *
- * You can use any conjunction operator valid for your Field's data type (see "Field
- * Data Types" section).
- *
- * ## Value vs Extra
- *
- * In the "Entity Example" above you might notice that each field attached to
- * entities has two properties that looks pretty similar, `value` and `extra`, as
- * both are intended to store information. Here we explain the "why" of this.
- *
- * ### Field Data Types
- *
- * Field must store information using basic data types such as (int, decimal, etc),
- * this information will be stored in a cells specific to that data type. Supported
- * data types are:
- *
- * - datetime: For storage of date or datetime values.
- * - decimal: For storage of floating values.
- * - int: For storage of integer values.
- * - text: For storage of long strings.
- * - varchar: For storage of strings maximum to 255 chars length.
- *
- * But in some cases Field Handlers may store complex information or structures not
- * supported by the basic types listed above, for instance collections of values,
- * objects, etc.
- *
- * ### Example
- *
- * For example, an `AlbumField` handler may store a list of photos for each entity.
- * In those cases you should use the `extra` property to store your array list of
- * photos. We could store an array list of file names and titles for a given entity
- * under the `extra` property, and we could save photo’s titles as space- separated
- * values under `value` property:
- *
- * ```php
- * // extra:
- * [photos] => [
- *     ['title' => 'OMG!', 'file' => 'omg.jpg'],
- *     ['title' => 'Look at this, lol', 'file' => 'cats-fighting.gif'],
- *     ['title' => 'Fuuuu', 'file' => 'fuuuu-meme.png'],
- * ]
- *
- * // value: OMG! Look at this lol Fuuuu
- * ```
- *
- * In our example when rendering an entity with `AlbumField` attached to it,
- * `AlbumField` should use `extra` information to create a representation of itself,
- * while `value` information would acts like some kind of `words index` when using
- * `Searching over custom fields` feature described above.
- *
- * **Important:**
- *
- * -  FieldableBehavior automatically serializes & unserializes the `extra` property
- *    for you, so you should always treat `extra` as an array or object.
- *
- * -  `Search over custom fields` feature described above uses the `value` property
- *    when looking for matches. So in this way your entities can be found when using
- *    Field’s machine-name in WHERE clauses.
- *
- * **Summarizing:** `value` is intended to store basic typed information suitable
- * for searches, while `extra` CAB be used to store sets of complex information.
- *
- * ***
- *
- * ## Using this behavior
- *
- * Just like any other behavior, in your Table constructor attach this behavior
- * as usual:
- *
- * ```php
- * $this->attachBehavior('Field.Fieldable');
- * ```
- *
- * ## Enable/Disable Field Attachment
- *
- * If for some reason you don't need custom fields to be fetched under the `_field`
- * of your entities you should use the unbindFieldable(). Or bindFieldable() to
- * enable it again.
- *
- * ```php
- * // there wont be a "_field" key on your User entity
- * $this->User->unbindFieldable();
- * $this->Users->get($id);
- * ```
- *
- * ## About Field Handlers
- *
- * Field Handler are "Listeners" classes which must take care of storing, organizing
- * and retrieving information for each entity's field. All this is archived using
- * QuickAppsCMS's events system
- *
- * Similar to Event Listeners and Hooktags, Field Handlers classes
- * must define a series of events, which has been organized in two groups or
- * "event subspaces":
- *
- * - `Field.<FieldHandler>.Entity`: For handling Entity's related events such
- *    as `entity save`, `entity delete`, etc.
- * - `Field.<FieldHandler>.Instance`: Related to Field Instances events, such as
- *    "instance being detached from table", "new instance attached to table", etc.
- *
- * Below, a list of available events:
- *
- * **Entity events:**
- *
- * **NOTE:** In order to make reading more comfortable the
- * `Field.<FieldHandler>.Entity.` prefix has been trimmed from each event
- * name listed below. For example, `display` is actually
- * `Field.Field.<FieldHandler>.Entity.info`
- *
- * - `display`: When an entity is being rendered.
- * - `edit`: When an entity is being rendered in `edit` mode. (backend usually).
- * - `fieldAttached`: When a field is attached to entity's "_field" property.
- * - `beforeFind`: Before an entity is retrieved from DB.
- * - `beforeValidate`: Before entity is validated as part of save operation.
- * - `afterValidate`: After entity is validated as part of save operation.
- * - `beforeSave`: Before entity is saved.
- * - `afterSave`: After entity was saved.
- * - `beforeDelete`: Before entity is deleted.
- * - `afterDelete`: After entity was deleted.
- *
- * ***
- *
- * **Instance events:**
- *
- * **NOTE:** In order to make reading more comfortable the
- * `Field.<FieldHandler>.Instance.` prefix has been trimmed from each event
- * name listed below. For example, `info` is actually
- * `Field.<FieldHandler>.Instance.info`
- *
- * - `info`: When QuickAppsCMS asks for information about each registered Field.
- * - `settingsForm`: Additional settings for this field, should define the way
- *    the values will be stored in the database.
- * - `settingsDefaults`: Default values for field settings form's inputs.
- * - `settingsValidate`: Before instance's settings are changed, here you can
- *    apply your own validation rules.
- * - `viewModeForm`: Additional view mode settings, should define the way the
- *    values will be rendered for a particular view mode.
- * - `viewModeDefaults`: Default values for view mode settings form's inputs.
- * - `viewModeValidate`: Before view-mode's settings are changed, here you can
- *    apply your own validation rules.
- * - `beforeAttach`: Before field is attached to Tables.
- * - `afterAttach`: After field is attached to Tables.
- * - `beforeDetach`: Before field is detached from Tables.
- * - `afterDetach`: After field is detached from Tables.
- *
- *
- * ## Preparing Field Inputs
- *
- * Your Field Handler should somehow render some form elements (inputs, selects,
- * textareas, etc) when rendering Table Entities in `edit mode`. For this we have
- * the `Field.<FieldHandler>.Entity.edit` event, which should return a HTML
- * containing all the form elements for [entity, field_instance] tuple.
- *
- * For example, lets suppose we have a `TextField` attached to `Users` Table for
- * storing their `favorite_food`, and now we are editing some specific `User`
- * Entity (i.e.: User.id = 4), so in the form editing page we should see some
- * inputs for change some values like `username` or `password`, and also we
- * should see a `favorite_food` input where Users shall type in their favorite
- * food. Well, your TextField Handler should return something like this:
- *
- * ```html
- * <!-- note the ":" prefix -->
- * <input name=":favorite_food" value="<current_value_from_entity>" />
- * ```
- *
- * To accomplish this, your Field Handler should properly catch the
- * `Field.<FieldHandler>.Entity.edit` event, example:
- *
- * ```php
- * public function entityEdit(Event $event, $field)
- * {
- *     return '<input name=":' . $field->name . '" value="' . $field->value . '" />";
- * }
- * ```
- *
- * As usual, the second argument `$field` contains all the information you will
- * need to properly render your form inputs.
- *
- * You must tell to QuickAppsCMS that the fields you are sending in your POST
- * action are actually virtual fields. To do this, all your input's `name`
- * attributes **must be prefixed** with `:` followed by its machine
- * (a.k.a. `slug`) name:
- *
- * ```html
- * <input name=":<machine-name>" ... />
- * ```
- *
- * You may also create complex data structures like so:
- *
- * ```html
- * <input name=":album.name" value="<current_value>" />
- * <input name=":album.photo.0" value="<current_value>" />
- * <input name=":album.photo.1" value="<current_value>" />
- * <input name=":album.photo.2" value="<current_value>" />
- * ```
- *
- * The above may produce a $_POST array like below:
- *
- * ```php
- * $_POST = [
- *     :album => [
- *         name => Album Name,
- *         photo => [
- *             0 => url_image1.jpg,
- *             1 => url_image2.jpg,
- *             2 => url_image3.jpg
- *         ]
- *     ],
- *     ...
- *     :other_field => ...,
- * ]
- * ```
- *
- * **Remember**, you should always rely on View::elements() for rendering HTML code:
- *
- * ```php
- * public function editTextField(Event $event, $field) {
- *     $view = $event->subject();
- *     return $View->element('text_field_edit', ['field' => $field]);
- * }
- * ```
- *
- * ## Creating an Edit Form
- *
- * In previous example we had an User edit form. When rendering User's form-inputs
- * usually you would do something like so:
- *
- * ```php
- * // edit.ctp
- * <?php echo $this->Form->input('id', ['type' => 'hidden']); ?>
- * <?php echo $this->Form->input('username'); ?>
- * <?php echo $this->Form->input('password'); ?>
- * ```
- *
- * When rendering virtual fields you can pass the whole Field Object to
- * `FormHelper::input()` method. So instead of passing the input name as first
- * argument (as above) you can do as follow:
- *
- * ```php
- * // Remember, custom fields are under the `_fields` property of your entity
- * <?php echo $this->Form->input($user->_fields[0]); ?>
- * <?php echo $this->Form->input($user->_fields[1]); ?>
- * ```
- *
- * **TIP:** You can get any custom field by using either, its index or its machine
- * name, for example: `$user->_field['user-age']` is the same as `$user->_field[0]`
- *
- * That will render the first and second virtual field attached to your entity.
- * But usually you'll end creating some loop structure and render all of them
- * at once:
- *
- * ```php
- * <?php foreach ($user->_fields as $field): ?>
- *     <?php echo $this->Form->input($field); ?>
- * <?php endforeach; ?>
- * ```
- *
- * As you may see, `Form::input()` **automagically fires** the
- * `Field.<FieldHandler>.Entity.edit` event asking to the corresponding Field
- * Handler for its HTML form elements. Passing the Field object to `Form::input()`
- * is not mandatory, you can manually generate your input elements:
- *
- * ```php
- * <input name=":<?php echo $field->name; ?>" value="<?php echo $field->value; ?>" />
- * ```
- *
- * The `$user` variable used in these examples assumes you used `Controller::set()`
- * method in your controller.
- *
- * A more complete example:
- *
- * ```php
- * // UsersController.php
- * public function edit($id)
- * {
- *     $this->set('user', $this->Users->get($id));
- * }
- *
- * // edit.ctp
- * <?php echo $this->Form->create($user); ?>
- *     <?php echo $this->Form->hidden('id'); ?>
- *     <?php echo $this->Form->input('username'); ?>
- *     <?php echo $this->Form->input('password'); ?>
- *     <!-- Custom Fields -->
- *     <?php foreach ($user->_fields as $field): ?>
- *         <?php echo $this->Form->input($field); ?>
- *     <?php endforeach; ?>
- *     <!-- /Custom Fields -->
- *     <?php echo $this->Form->submit('Save User'); ?>
- * <?php echo $this->Form->end(); ?>
- * ```
+ * @link https://github.com/quickapps/docs/blob/2.x/en/developers/field-api.rst
  */
 class FieldableBehavior extends Behavior
 {
@@ -514,7 +120,6 @@ class FieldableBehavior extends Behavior
      */
     public function __construct(Table $table, array $config = [])
     {
-        $this->_table = $table;
         if (empty($config['tableAlias'])) {
             $config['tableAlias'] = Inflector::underscore($table->alias());
         }
@@ -584,11 +189,11 @@ class FieldableBehavior extends Behavior
      *
      * @param \Cake\Event\Event $event The beforeFind event that was triggered
      * @param \Cake\ORM\Query $query The original query to modify
-     * @param array $options Additional options given as an array
+     * @param \ArrayObject $options Additional options given as an array
      * @param bool $primary Whether this find is a primary query or not
      * @return bool|null
      */
-    public function beforeFind(Event $event, Query $query, $options, $primary)
+    public function beforeFind(Event $event, Query $query, ArrayObject $options, $primary)
     {
         if ((isset($options['fieldable']) && $options['fieldable'] === false) ||
             !$this->config('enabled')
@@ -1111,9 +716,7 @@ class FieldableBehavior extends Behavior
         list(, $driverClass) = namespaceSplit(strtolower(get_class($conn->driver())));
 
         $whereClause->traverse(function ($expression) use ($pk, $alias, $driverClass) {
-            if (!($expression instanceof Comparison) ||
-                !($subQuery = $this->_virtualQuery($expression))
-            ) {
+            if (!($subQuery = $this->_virtualQuery($expression))) {
                 return;
             }
 
@@ -1148,6 +751,10 @@ class FieldableBehavior extends Behavior
      */
     protected function _virtualQuery($expression, $bundle = null)
     {
+        if (!($expression instanceof Comparison)) {
+            return false;
+        }
+
         $field = $this->_parseFieldName($expression->getField());
         $value = $expression->getValue();
         $conjunction = $expression->getOperator();
