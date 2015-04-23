c9dbcadcf9188a2725adea3f587a3a9a51c66370
QuickApps <info@quickapps.es>
2015-04-23 15:35:13 +0200

    adding Menu test cases

---
+++ plugins/Menu/tests/TestCase/Controller/Admin/ManageControllerTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since    2.0.0
+ * @author   Christopher Castro <chris@quickapps.es>
+ * @link     http://www.quickappscms.org
+ * @license  http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+namespace Menu\Test\TestCase\Controller\Admin;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * ManageControllerTest class.
+ */
+class ManageControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures.
+     *
+     * @var array
+     */
+    public $fixtures = [
+        //'app.acos',
+        'app.block_regions',
+        'app.blocks',
+        'app.blocks_roles',
+        'app.comments',
+        'app.eav_attributes',
+        'app.eav_values',
+        'app.entities_terms',
+        'app.field_instances',
+        'app.languages',
+        'app.menu_links',
+        'app.menus',
+        'app.node_revisions',
+        'app.nodes',
+        'app.nodes_roles',
+        'app.node_types',
+        'app.options',
+        'app.permissions',
+        'app.plugins',
+        'app.roles',
+        'app.search_datasets',
+        'app.terms',
+        'app.users',
+        'app.users_roles',
+        'app.vocabularies',
+    ];
+
+    /**
+     * setUp().
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->session(mockUserSession());
+    }
+
+    /**
+     * test index action.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->get('/admin/menu/manage');
+        $this->assertResponseOk();
+    }
+
+    /**
+     * test add action.
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->post('/admin/menu/manage/add', [
+            'title' => 'Test Menu',
+            'description' => 'this is a test menu',
+        ]);
+        $menu = $this->_controller
+            ->Menus
+            ->find()
+            ->where(['title' => 'Test Menu'])
+            ->limit(1)
+            ->first();
+        $this->assertNotEmpty($menu);
+    }
+}
