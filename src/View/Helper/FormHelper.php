4cdd65e98355ba4e325af58279a442ffa3ca2556
QuickApps <info@quickapps.es>
2015-04-30 05:20:41 +0200

    refactoring some logic using new Aspects API

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -56,15 +56,6 @@ class FormHelper extends CakeFormHelper
     /**
      * {@inheritDoc}
      */
-    public function label($fieldName, $text = null, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::label($fieldName, $text, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public function input($fieldName, array $options = [])
     {
         if ($fieldName instanceof \Field\Model\Entity\Field) {
@@ -82,153 +73,7 @@ class FormHelper extends CakeFormHelper
                 return $this->input($fieldName->name, $options);
             }
         }
-
-        $fieldName = $this->_fieldName($fieldName);
         return parent::input($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function checkbox($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::checkbox($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function radio($fieldName, $options = [], array $attributes = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::radio($fieldName, $options, $attributes);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function textarea($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::textarea($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function hidden($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::hidden($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function file($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::file($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function select($fieldName, $options = [], array $attributes = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::select($fieldName, $options, $attributes);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function multiCheckbox($fieldName, $options, array $attributes = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::multiCheckbox($fieldName, $options, $attributes);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function day($fieldName = null, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::day($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function year($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::year($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function month($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::month($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function hour($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::hour($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function minute($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::minute($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function meridian($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::meridian($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function dateTime($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::dateTime($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function time($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::time($fieldName, $options);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function date($fieldName, array $options = [])
-    {
-        $fieldName = $this->_fieldName($fieldName);
-        return parent::date($fieldName, $options);
     }
 
     /**
@@ -259,20 +104,5 @@ class FormHelper extends CakeFormHelper
             $this->_prefix = $prefix;
         }
         return $this->_prefix;
-    }
-
-    /**
-     * Add prefix to field name if a prefix was set using prefix().
-     *
-     * @param string $name Field name
-     * @return string Prefixed field name
-     */
-    protected function _fieldName($name)
-    {
-        if (!empty($this->_prefix) && strpos($name, $this->_prefix) !== 0) {
-            $name = "{$this->_prefix}{$name}";
-        }
-
-        return $name;
     }
 }
