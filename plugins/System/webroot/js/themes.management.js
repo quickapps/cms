838ab393bf0dd09e1f134a0b4ed0ae5d01e70a26
QuickApps <info@quickapps.es>
2015-04-30 11:52:47 +0200

    fix minor js glitch

--- plugins/System/webroot/js/themes.management.js
+++ plugins/System/webroot/js/themes.management.js
@@ -1,5 +1,7 @@
 function filterBy(type) {
     var type = type.replace('#show-', '');
+    type = type == '' ? 'front' : type;
+
     $('.filters a.btn').removeClass('active');
     $('.filters a.btn-' + type).addClass('active');
     $('.themes-list').hide();
