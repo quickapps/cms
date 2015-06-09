9bbfbdb5ffd8b4bb34e85bff1da8c5f0acff479e
QuickApps <info@quickapps.es>
2015-06-09 10:02:17 +0200

    minor fix
    [skip ci]

--- plugins/Search/src/Model/Behavior/SearchableBehavior.php
+++ plugins/Search/src/Model/Behavior/SearchableBehavior.php
@@ -529,7 +529,7 @@ class SearchableBehavior extends Behavior
     /**
      * Generates default callable object for extracting entity's words.
      *
-     * @return callable
+     * @return \Closure
      */
     protected function _defaultFieldsCallable()
     {
@@ -770,7 +770,7 @@ class SearchableBehavior extends Behavior
         $text = trim(preg_replace('/\s{2,}/i', ' ', $text)); // remove double spaces
         $text = strtolower($text); // all to lowercase
         $text = $this->_filterText($text); // filter
-        $text = iconv("UTF-8", "UTF-8//IGNORE", $text); // remove any invalid character
+        $text = iconv('UTF-8', 'UTF-8//IGNORE', mb_convert_encoding($text, 'UTF-8')); // remove any invalid character
         return trim($text);
     }
 
